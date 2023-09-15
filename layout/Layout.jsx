3c60c067243aaddd41a123a80b2a4aeb512569dd
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-15 15:52:55 -0600

    Created: layout

---
+++ layout/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { Sidebar } from "../components";
+
+
+export default function Layout({ children, title = '', description = '' }) {
+  return (
+    <>
+      <Head>
+        <title>{`GuitarLA - ${title}`}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/vilg-favicon.png" />
+      </Head>
+
+      <div className="md:flex">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+          <Sidebar />
+        </aside>
+
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          {children}
+        </main>
+      </div>
+    </>
+  )
+}
--- pages/index.js
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold text-center mt-5">Hello World</h1>
-  )
-}
