efa207a46ab818ddb25a88dd740d1b98cf1a9561
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-15 18:27:46 -0600

    Added: currentCategory functionality

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
+import { useKiosk } from '../hooks/useKiosk';
 
 export function Category({ category }) {
+
+  const { currentCategory, onClickCategory } = useKiosk();
 
   const { id, name, icon } = category;
 
   return (
-    <div className='flex items-center gap-4 w-full border p-5 hover:bg-amber-400 transition-colors'>
-       <Image
+    <div className={`${(currentCategory?.id === id) ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border p-5 hover:bg-amber-400 transition-colors`}>
+      <Image
         src={`/assets/img/icono_${icon}.svg`}
         width={70}
         height={70}
@@ -15,6 +18,7 @@ export function Category({ category }) {
 
       <button
         className='text-2xl font-bold'
+        onClick={() => onClickCategory(id)}
       >
         {name}
       </button>
--- context/KioskProvider.jsx
+++ context/KioskProvider.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
 import { useCategories } from '../hooks/useCategories';
 import { KioskContext } from './KioskContext';
 
 export function KioskProvider({ children }) {
   const { categories } = useCategories();
+  const [currentCategory, setCurrentCategory] = useState({});
+
+  const onClickCategory = (id) => {
+    const [category] = categories.filter(cat => cat.id === id);
+    setCurrentCategory(category);
+  }
+
+  useEffect(() => {
+    setCurrentCategory(categories[0])
+  }, [categories])
 
   return (
     <KioskContext.Provider
       value={{
-        categories
+        categories,
+        currentCategory,
+        onClickCategory
       }}
     >
       {children}
