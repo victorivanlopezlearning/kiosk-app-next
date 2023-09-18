cb6fbda9c0ec05107cc8bfae5623c349a8a6f9fd
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-18 12:55:03 -0600

    Updated: code formatted

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -3,8 +3,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-
   const categories = await prisma.category.findMany();
-
   res.status(200).json({ categories })
 }
