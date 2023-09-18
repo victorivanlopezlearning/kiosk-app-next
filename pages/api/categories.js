8bc9aad775e9e1e526509883922c6fe7af0a28d0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-18 13:06:11 -0600

    updated: include related data

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -3,6 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const categories = await prisma.category.findMany();
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true
+    }
+  });
   res.status(200).json({ categories })
 }
