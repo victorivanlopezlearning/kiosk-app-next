76b22e5e258c56b193899eaf956a903acd526721
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-18 18:49:07 -0600

    Configured: modal functionality

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,25 +1,40 @@
 import Image from 'next/image';
 import React from 'react'
 import { formatToDollars } from '../helpers';
+import { useKiosk } from '../hooks/useKiosk';
 
 export function Product({ product }) {
 
-  const { id, name, price, image } = product;
+  const { onSetProduct, onSetModal } = useKiosk()
+
+  const { name, price, image } = product;
 
   return (
-    <div className='border p-3'>
+    <div className='border p-3 flex flex-col justify-between'>
       <Image
         src={`/assets/img/${image}.jpg`}
         width={400}
         height={500}
         alt={`Imagen de ${name}`}
       />
-
-      <div className="p-5">
+      <div className='px-5 pt-5 mb-auto'>
         <h3 className='text-2xl font-bold'>{name}</h3>
+      </div>
+
+      <div className="px-5">
         <p className='mt-5 font-black text-4xl text-amber-500'>
           {formatToDollars(price)}
         </p>
+
+        <button
+          className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 font-bold transition-colors'
+          onClick={() => {
+            onSetModal();
+            onSetProduct(product);
+          }}
+        >
+          Agregar
+        </button>
       </div>
     </div>
   )
--- context/KioskProvider.jsx
+++ context/KioskProvider.jsx
@@ -5,22 +5,36 @@ import { KioskContext } from './KioskContext';
 export function KioskProvider({ children }) {
   const { categories } = useCategories();
   const [currentCategory, setCurrentCategory] = useState({});
+  const [product, setProduct] = useState({});
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    setCurrentCategory(categories[0])
+  }, [categories])
 
   const onClickCategory = (id) => {
     const [category] = categories.filter(cat => cat.id === id);
     setCurrentCategory(category);
   }
 
-  useEffect(() => {
-    setCurrentCategory(categories[0])
-  }, [categories])
+  const onSetProduct = (product) => {
+    setProduct(product);
+  }
+
+  const onSetModal = () => {
+    setModal(!modal);
+  }
 
   return (
     <KioskContext.Provider
       value={{
         categories,
         currentCategory,
-        onClickCategory
+        onClickCategory,
+        onSetProduct,
+        product,
+        onSetModal,
+        modal
       }}
     >
       {children}
