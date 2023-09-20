96e3ae7f36347970ab57c1d3bce3ef2882253006
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-20 13:55:54 -0600

    Created: ProductSummary component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 export * from './Category';
+export * from './Message';
 export * from './ModalProduct';
 export * from './Product';
+export * from './ProductSummary';
 export * from './Sidebar';
 export * from './Steps';
