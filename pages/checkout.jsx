b35de570ac6472296805824655773a8faf6163c0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-19 19:15:27 -0600

    Component created

---
+++ pages/checkout.jsx
@@ -0,0 +1,10 @@
+import Layout from '../layout/Layout';
+
+
+export default function Checkout() {
+  return (
+    <Layout>
+      <h1>Checkout</h1>
+    </Layout>
+  )
+}
---
+++ pages/summary.jsx
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout';
+
+
+export default function Sumary() {
+  return (
+    <Layout title='Resumen del pedido'>
+      <h1 className='text-4xl font-black'>Resumen</h1>
+      <p className='text-2xl my-10'>Revisa tu pedido</p>
+    </Layout>
+  )
+}
